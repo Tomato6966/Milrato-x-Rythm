af57350ffe871af49460c1e4d03d9296b1744b62
Tomato6966 <68145571+Tomato6966@users.noreply.github.com>
2021-03-31 15:37:17 +0200

    Add files via upload

--- handlers/functions.js
+++ handlers/functions.js
@@ -43,7 +43,7 @@ module.exports = {
     const min = Math.floor((ms / (60 * 1000)) % 60).toString();
     const hrs = Math.floor((ms / (60 * 60 * 1000)) % 60).toString();
     const days = Math.floor((ms / (24 * 60 * 60 * 1000)) % 60).toString();
-    return `${days}Days,${hrs}Hours,${min}Minutes,${sec}Seconds`;
+    return `\`${days} Days\`, \`${hrs} Hours\`, \`${min} Minutes\`, \`${sec} Seconds\``;
   },
   promptMessage: async function (message, author, time, validReactions) {
     try {
